adf78d860c6b6041a828c0a6ada163e52b6f2c76
Marge <marge.consunji@gmail.com>
2022-06-15 12:26:51 +0800

    Add story to Transaction component

---
+++ src/components/Transaction/Transaction.stories.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Transaction from './Transaction';
+
+export default {
+  title: 'Transaction',
+  component: Transaction,
+};
+
+const Template = (args) => <Transaction {...args} />;
+
+export const Default = Template.bind({});
+
+Default.args = {
+  transaction: {
+    transactionName: 'Cat food',
+    amount: 1200,
+    type: 'expense',
+    id: 1,
+  },
+};
