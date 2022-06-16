af48dd456c9ccab4d7478b165a2fd5f5b54a828f
Marge <marge.consunji@gmail.com>
2022-06-16 09:08:29 +0800

    Remove dummyData array

--- src/context/context.js
+++ src/context/context.js
@@ -3,27 +3,6 @@ import { createContext, useState } from 'react';
 const TransactionContext = createContext();
 
 const TransactionProvider = ({ children }) => {
-  const dummyData = [
-    {
-      transactionName: 'Cat food',
-      amount: 1200,
-      type: 'expense',
-      id: 1,
-    },
-    {
-      transactionName: 'Income',
-      amount: 13000,
-      type: 'income',
-      id: 1,
-    },
-    {
-      transactionName: 'Phone bill - June',
-      amount: 1400,
-      type: 'expense',
-      id: 1,
-    },
-  ];
-
   const [transactions, setTransactions] = useState([]);
   const [openModal, setOpenModal] = useState(false);
   const [modalType, setModalType] = useState();
