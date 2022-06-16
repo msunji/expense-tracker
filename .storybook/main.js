15767d2e7a8c8e529b17fc3f6f3b506e50651427
Marge <marge.consunji@gmail.com>
2022-06-16 09:43:47 +0800

    Removed unused block of code

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,25 +6,17 @@ module.exports = {
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
     '@storybook/preset-create-react-app',
+    {
+      name: '@storybook/addon-postcss',
+      options: {
+        postcssLoaderOptions: {
+          implementation: require('postcss'),
+        },
+      },
+    },
   ],
   framework: '@storybook/react',
   core: {
     builder: '@storybook/builder-webpack5',
-  },
-  webpackFinal: async (config) => {
-    config.module.rules.push({
-      test: /\,css&/,
-      use: [
-        {
-          loader: 'postcss-loader',
-          options: {
-            ident: 'postcss',
-            plugins: [require('tailwindcss'), require('autoprefixer')],
-          },
-        },
-      ],
-      include: path.resolve(__dirname, '../'),
-    });
-    return config;
   },
 };
