4f99a860f64e4f4e3dbf5d696457b018f7aed759
Marge <marge.consunji@gmail.com>
2022-06-24 14:56:16 +0800

    Export basic form story

--- src/components/TransactionForm/TransactionForm.stories.js
+++ src/components/TransactionForm/TransactionForm.stories.js
@@ -1,2 +1,9 @@
-import React from 'react';
 import TransactionForm from './TransactionForm';
+
+export default {
+  title: 'TransactionForm',
+  component: TransactionForm,
+};
+const Template = (args) => <TransactionForm {...args} />;
+
+export const Primary = Template.bind({});
